d9e92d7c3074d6af47aff359c6262894ed16eccd
evday <1194984735@qq.com>
2017-12-16 10:44:30 +0800

    课程详情页面收藏功能完成

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -8,6 +8,7 @@ from django.views.generic.base import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Course
+from operation.models import UserFavorite
 
 class CourseListView(View):
     def get(self,request):
@@ -44,6 +45,17 @@ class CourseDetailView(View):
         course.click_nums += 1
         course.save()
 
+        has_fav_course = False
+        has_fav_org = False
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user,fav_id=course_id,fav_type=1):
+                has_fav_course = True
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.course_org.id, fav_type=2):
+                has_fav_org = True
+
+
+
+
         tag = course.tag
         if tag:
             related_courses = Course.objects.filter(tag=tag)[:2]
@@ -51,5 +63,7 @@ class CourseDetailView(View):
             related_courses = []
         return render(request,'course-detail.html',{
             "course":course,
-            "related_courses":related_courses
+            "related_courses":related_courses,
+            "has_fav_course":has_fav_course,
+            "has_fav_org":has_fav_org,
         })
